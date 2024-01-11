3e278b9d441afd7849eef0e19fb293a1fcb47b2e
scially <imhwang@126.com>
2024-01-11 18:26:00 +0800

    [fix] memory overload

--- include/Commons/Version.h
+++ include/Commons/Version.h
@@ -2,7 +2,7 @@
 
 #define SCIALLY_PROJECT_VERSION_MAJOR 3
 #define SCIALLY_PROJECT_VERSION_MINOR 1
-#define SCIALLY_PROJECT_VERSION_PATCH 1
+#define SCIALLY_PROJECT_VERSION_PATCH 2
 
-constexpr const char* SCIALLY_PROJECT_VERSION = "3.1.1";
+constexpr const char* SCIALLY_PROJECT_VERSION = "3.1.2";
 constexpr const char* SCIALLY_PROJECT_NAME = "Cesium3DTilesConverter";
